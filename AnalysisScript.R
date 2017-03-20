e65c55f00d0625ecfe43546557e46ecf44e7e3b3
flaxmans <sam.flaxman@gmail.com>
2017-03-20 14:22:16 -0600

    added Holekamp et al's fig 6 data and started analysis script

---
+++ AnalysisScript.R
@@ -0,0 +1,9 @@
+# Script for analyzing data from Holekamp_et_al figure 6
+
+# set the working directory:
+setwd("~/playground/CollaborativeRepo/")
+
+# import data:
+fig6data <- read.csv("HolekampData/Holekampetal_data_Fig6.csv")
+
+# let's do some great stuff
