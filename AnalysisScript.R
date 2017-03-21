60592615955ac4cbc368076ebce622044444e6a1
flaxplus <sam.flaxman@protonmail.com>
2017-03-21 00:03:55 -0600

    added lines of code to get IDs from network data

--- AnalysisScript.R
+++ AnalysisScript.R
@@ -1,9 +1,15 @@
 # Script for analyzing data from Holekamp_et_al figure 6
 
-# set the working directory:
-setwd("~/playground/CollaborativeRepo/")
+# set the working directory for your machine:
+# setwd("~/path/to/CollaborativeRepo/")
 
 # import data:
 fig6data <- read.csv("HolekampData/Holekampetal_data_Fig6.csv")
 
 # let's do some great stuff
+# let's figure out how many individuals there are:
+# need to convert to chars 
+IDs1 <- unique(as.character(fig6data$firstID))
+IDs2 <- unique(as.character(fig6data$secondID))
+allIDs <- unique(c(IDs1, IDs2))
+numIndividuals <- length(allIDs)
